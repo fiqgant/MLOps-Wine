968da4b6c30f198c3b0c0bc0651dac85f4d56696
Taufiqurrahman <64482868+fiqgant@users.noreply.github.com>
2022-08-16 12:55:49 +0700

    Update train.py

--- train.py
+++ train.py
@@ -3,7 +3,7 @@ import seaborn as sns
 import pandas as pd 
 import numpy as np
 
-from sklearn.ensemble import RandomForestranforessor
+from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
 
 
@@ -21,7 +21,7 @@ X_train, X_test, y_train, y_test = train_test_split(df, y, test_size=0.2, random
 
 
 # Fit a model on the train section
-ranfor = RandomForestranforessor(max_depth=2, random_state=seed)
+ranfor = RandomForestRegressor(max_depth=2, random_state=seed)
 ranfor.fit(X_train, y_train)
 
 
