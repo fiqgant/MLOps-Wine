48ceec103850380838126ac87bbcb39491de941e
Taufiqurrahman <64482868+fiqgant@users.noreply.github.com>
2022-08-16 13:17:37 +0700

    max_depth=7

--- train.py
+++ train.py
@@ -21,7 +21,7 @@ X_train, X_test, y_train, y_test = train_test_split(df, y, test_size=0.2, random
 
 
 # Fit a model on the train section
-ranfor = RandomForestRegressor(max_depth=2, random_state=seed)
+ranfor = RandomForestRegressor(max_depth=7, random_state=seed)
 ranfor.fit(X_train, y_train)
 
 
